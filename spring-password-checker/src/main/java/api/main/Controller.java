dad7d5705e7d8cdd2e0a89093cc707d95b730b01
Sandipan Kundu <sk.tech.1997@outlook.com>
2024-02-11 13:26:20 +0530

    URL path addition for swagger ui

--- spring-password-checker/src/main/java/api/main/Controller.java
+++ spring-password-checker/src/main/java/api/main/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/* Url : http://localhost:8080/swagger-ui/index.html */
 @RestController
 @RequestMapping("/api")
 public class Controller {
